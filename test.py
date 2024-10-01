ed2c40c024c183922cd624a1d209442d8579aa5b
moti <m0583222683@gmail.com>
2024-10-01 12:08:15 +0300

    tear

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
+from datetime import datetime
+current_year = datetime.now().year
+print(current_year)
+
+
 class student:
     def __init__(self, name, year, id):
         self.name = name
         self.year = year
         self.id = id
+
+    def year(self):
+       return datetime.now().year - self.year
+
+
+
+
 
