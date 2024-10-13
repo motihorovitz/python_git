975c60b2e96969e8aad61532da6b4d5218205768
moti <m0583222683@gmail.com>
2024-10-13 09:44:43 +0300

    init

---
+++ test.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+class Student:
+    def __init__(self, name, year, id):
+        self.name = name
+        self.year = year
+        self.id = id
+
+    def c_year(self):
+       return datetime.now().year - self.year
+
+    def list_stu(self):
+        return len(self.name)
+
+student = Student("moti", 2001, 2122345)
+print(student.c_year())
+
+print("g")
+
+
+
